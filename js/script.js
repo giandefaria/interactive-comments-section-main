0dee520640feabbd9705ad5d80200418d11f14c8
Gian <gian.defaria@hotmail.com>
2022-12-02 03:37:07 -0300

    limpando o código e melhor organizando

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const button = document.querySelectorAll(".button");
 const buttonPlus = document.querySelectorAll(".plus");
-const buttonMinus= document.querySelectorAll(".minus");
+const buttonMinus = document.querySelectorAll(".minus");
 const ratingValue = document.querySelectorAll(".rating--value");
 let edit = document.querySelectorAll(".edit");
 let reply = document.querySelectorAll(".reply");
@@ -12,9 +12,6 @@ let id = 1;
 let mention;
 
 for (let i = 0; i < button.length; i++) {
-  
-    //array.push(i)
-    //console.log(array[i]);
 
     button[i].addEventListener('click', (e) => {
         let newDiv = document.createElement("div");
@@ -30,7 +27,7 @@ for (let i = 0; i < button.length; i++) {
             <button class="send update insert" onclick="saveComent()" type="submit">REPLY</button>
         </form>
         `;
-  
+
         //puxei os parametros informados pelo evento e com ele descobri que consigo selecionar o box em que foi apertado o botão pela função path[1]
         //adicionei o comando para adicionar a nova div após o path[1]
         console.log(e);
@@ -45,14 +42,14 @@ for (let i = 0; i < button.length; i++) {
 let send = document.querySelectorAll(".comment--up");
 
 //cria o box do comentário principal
-    for (let i = 0; i < send.length; i++) {
-   
+for (let i = 0; i < send.length; i++) {
+
     send[i].addEventListener('click', (e) => {
 
-    let newDiv = document.createElement("div");
-    newDiv.className = "col-sm-12 mb-4 teste display";
-    newDiv.id = "user--coment";
-    newDiv.innerHTML = `
+        let newDiv = document.createElement("div");
+        newDiv.className = "col-sm-12 mb-4 teste display";
+        newDiv.id = "user--coment";
+        newDiv.innerHTML = `
     
     <div class="container--coment box paddingBox">
         <div class="rating rating--desktop">
@@ -80,20 +77,19 @@ let send = document.querySelectorAll(".comment--up");
         </div> 
     </div>   
     `;
-    document.querySelector("body > main > .sendBox").before(newDiv);
-    console.log(e);
-    let event = e;
-    mention = event.path[2].id;
-    console.log(mention);
-    loop();
-    setUserName();
-    createUserComment();
-});  
-};  
-
+        document.querySelector("body > main > .sendBox").before(newDiv);
+        console.log(e);
+        let event = e;
+        mention = event.path[2].id;
+        console.log(mention);
+        loop();
+        setUserName();
+        createUserComment();
+    });
+};
 
 //cria o comentário do usuário principal
-function createUserComment () {
+function createUserComment() {
 
     let saveButton = document.querySelectorAll(".send--juliu");
     for (let i = 0; i < saveButton.length; i++) {
@@ -114,8 +110,7 @@ function createUserComment () {
     }
 };
 
-
-function saveComent () {
+function saveComent() {
     let saveButton = document.querySelectorAll(".insert");
     for (let i = 0; i < saveButton.length; i++) {
         saveButton[i].addEventListener('click', (e) => {
@@ -126,22 +121,20 @@ function saveComent () {
             let caminho = event.path[2];
             let valueInput = document.getElementById("coment").value;
             localStorage.setItem(mention, valueInput);
-        
+
             if (valueInput == "") {
                 alert("Insert a message")
             } else {
-                 text(caminho, i);
-                 setUserName();
-                 //refreshArray();
-              }
+                text(caminho, i);
+                setUserName();
+                //refreshArray();
+            }
         })
-   
     }
-
 }
 
 //função que pega o comentário no localstorage e adiciona na div criada, através de um output
-function text (caminho, i) {
+function text(caminho, i) {
     console.log(caminho, i);
     let userReply = document.querySelectorAll(".userReply");
 
@@ -170,32 +163,27 @@ function text (caminho, i) {
             <span class="span"></span><output class="finalcoment insert"></output>
         </div>
     </div>
-        
     `;
-   
+
     let outputValue = document.querySelectorAll(".insert");
     let mentionValue = document.querySelectorAll(".span");
     console.log(outputValue);
 
     outputValue[editArray].innerHTML = localStorage.getItem(mention);
     mentionValue[editArray].innerHTML = "@" + mention + " ";
- 
-
-
     loop();
-    
+
 }
 
-
 //comando de edição, abre a caixa do input e adiciona o valor no localStorage 
-function editar () {
-    
+function editar() {
+
     for (let i = 0; i < edit.length; i++) {
-        
+
         edit[i].addEventListener('click', (e) => {
 
             let event = e;
-   
+
             console.log(event);
             let caminho = event.path[3];
             mention = event.path[5].id;
@@ -211,66 +199,54 @@ function editar () {
                 <button class="send update" onclick="update()" type="submit">UPDATE</button>
             </form>
             `;
-        
+
             let valueInput = document.getElementById("coment");
             valueInput.value = localStorage.getItem(mention);
             loop();
         });
     }
-}    
+}
 
-function update () {
+function update() {
     let updateButton = document.querySelectorAll(".update");
     for (let i = 0; i < updateButton.length; i++) {
         updateButton[i].addEventListener('click', (e) => {
 
-            
+
             let event = e;
             console.log(event);
             let caminho = event.path[3];
             let valueInput = document.getElementById("coment").value;
             localStorage.setItem(mention, valueInput);
-        
+
             if (valueInput == "") {
                 alert("Insert a message")
             } else {
-                 text(caminho, i);
-                 setUserName();
-                 //refreshArray();
-              }
-
-
+                text(caminho, i);
+                setUserName();
+                //refreshArray();
+            }
         })
-
-    
     }
-
-
-
 }
 
-
-
-function del ( ) {
+function del() {
     let element = document.querySelectorAll(".reply");
     let deleteButton = document.querySelectorAll(".delete")
     for (let i = 0; i < deleteButton.length; i++) {
         let deleteButton = document.querySelectorAll(".delete");
         //console.log(i);
         deleteButton[i].addEventListener('click', () => {
-        console.log(deleteButton[i]);
-        element[i].remove();
+            console.log(deleteButton[i]);
+            element[i].remove();
 
         })
-        
     }
-    
 };
-
 
 //increment value
 for (let i = 0; i < buttonPlus.length; i++) {
-    
+
     buttonPlus[i].addEventListener('click', () => {
 
         if (ratingValue[i].inert == false) {
@@ -279,17 +255,15 @@ for (let i = 0; i < buttonPlus.length; i++) {
             value = ratingValue[i].value;
             value++;
             ratingValue[i].innerHTML = value;
-            ratingValue[i].inert = true;   
-        } 
-
+            ratingValue[i].inert = true;
+        }
     })
-    
 }
 
 //decrement value
 
 for (let i = 0; i < buttonMinus.length; i++) {
-    
+
     buttonMinus[i].addEventListener('click', () => {
 
         if (ratingValue[i].inert == false) {
@@ -298,9 +272,7 @@ for (let i = 0; i < buttonMinus.length; i++) {
             value = ratingValue[i].value;
             value--;
             ratingValue[i].innerHTML = value;
-            ratingValue[i].inert = true;   
-        } 
-
+            ratingValue[i].inert = true;
+        }
     })
-    
 }
